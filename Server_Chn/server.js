7d9d4a37a237813ee4890d8806b62780f563ae94
Abdallahmadian <139226008+Abdallahmadian@users.noreply.github.com>
2025-08-31 12:51:28 +0300

    changes...

--- Server_Chn/server.js
+++ Server_Chn/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 // Import all routes
-import deleteTrainRoutes from "./Server_Chn/DeleteTrain.js";
-import getTrainRoutes from "./Server_Chn/getTrain.js";
-import getTrainByIdRoutes from "./Server_Chn/getTrainById.js";
-import putTrainByIdRoutes from "./Server_Chn/PutTrainById.js";
-import postTrainRoutes from "./Server_Chn/post.js";
+import deleteTrainRoutes from "./DeleteTrain.js";
+import getTrainRoutes from "./getTrain.js";
+import getTrainByIdRoutes from "./getTrainById.js";
+import putTrainByIdRoutes from "./PutTrainById.js";
+import postTrainRoutes from "./post.js";
 
 const app = express();
 const prisma = new PrismaClient();
--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+
 async function main() {
   const allUsers = await prisma.trip.findMany();
   console.log(allUsers);
+  app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 }
 
 main()
@@ -15,4 +20,6 @@ main()
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
+    
   });
+  
