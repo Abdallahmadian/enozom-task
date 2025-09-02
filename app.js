ae68f96599f362648747e76029abcf129714c942
Abdallahmadian <139226008+Abdallahmadian@users.noreply.github.com>
2025-09-02 12:21:08 +0300

    app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import trainRoutes from './routes/trainRoutes.js';
 import tripRoutes from './routes/tripRoutes.js';
 import cityRoutes from './routes/cityRoutes.js';
-//import alexToCairoTripRoutes from './routes/alexToCairoTripRoutes.js';
+
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/api/trains', trainRoutes);
 app.use('/api/trips', tripRoutes);
 app.use('/api/cities', cityRoutes);
-//app.use('/api/alex-to-cairo-trips', alexToCairoTripRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
